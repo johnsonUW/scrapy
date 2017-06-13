a1d2bbf1707fc3b63f377ce55e1397ece46a5ba9
johnsonUW <johnson19910605@gmail.com>
2017-06-13 02:00:53 -0700

    env

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\htf\desktop\search~1\scrapy\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Scripts/scrapyd-deploy.bat
@@ -0,0 +1,3 @@
+@echo off
+
+"C:\Users\HTF\Desktop\search engine\scrapy\Scripts\python.exe" "C:\Users\HTF\Desktop\search engine\scrapy\Scripts\scrapy-deploy" %1 %2 %3 %4 %5 %6 %7 %8 %9
